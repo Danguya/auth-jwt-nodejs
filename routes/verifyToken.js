2cf34765dbac98620924d2f4965a8a2370334fa9
danguya <daniel.yava16@gmail.com>
2020-10-06 14:49:41 +0100

    Verify Token

---
+++ routes/verifyToken.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+
+function auth(req, res, next){
+    const token = req.header('auth_token');
+    if(!token) return res.status(401).send('Acess Denied');
+
+    try {
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+        req.user = verified;
+    } catch (error) {
+        res.status(400).send('Invalid Token');
+    }
+}
